f1e687bc611771015a4e01947360fd9e2dfc4157
DootDoot4567 <dootdoot4567@gmail.com>
2025-06-24 00:49:07 +0000

    Fix importing issue

--- web-server/grading/api/views.py
+++ web-server/grading/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from drf_spectacular.utils import extend_schema, extend_schema_view
 
 @extend_schema_view(

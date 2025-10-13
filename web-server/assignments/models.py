5f009065d263de347f266a1fa00e7b342a6b219b
DootDoot4567 <dootdoot4567@gmail.com>
2025-10-13 02:39:07 +0000

    Add status field to Submissions table

--- web-server/assignments/models.py
+++ web-server/assignments/models.py
@@ -59,7 +59,8 @@ class Submission(models.Model):
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, related_name='submissions')
     submission_time = models.DateTimeField(auto_now_add=True)
     submission_file = models.FileField(upload_to="student-submissions/", blank=True, null=True)
-        
+    status = models.CharField(max_length=20, default="not submitted", blank=True, null=True)
+
     def __str__(self):
         return f"Submission by {self.student} for {self.assignment}"
 

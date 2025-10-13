ac32a256a3bc92363229b826b8464790eba03d15
DootDoot4567 <dootdoot4567@gmail.com>
2025-10-13 01:12:41 +0000

    Add storage class to storage backends file

--- web-server/grade_gator/storage_backends.py
+++ web-server/grade_gator/storage_backends.py
@@ -1,7 +1,10 @@
-from storages.backends.s3boto3 import S3Boto3Storage
+lfrom storages.backends.s3boto3 import S3Boto3Storage
 
 class UngradedSubmissionsStorage(S3Boto3Storage):
     bucket_name = 'ungraded-submissions'
 
 class ProfessorTestCasesStorage(S3Boto3Storage):
     bucket_name = 'professor-test-cases'
+
+class ManualUngradedStorage(S3Boto3Storage):
+     bucket_name = 'manual-ungraded-submissions'

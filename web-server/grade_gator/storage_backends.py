a658a06a221c7b085177c62023861b341217fda2
DootDoot4567 <dootdoot4567@gmail.com>
2025-12-01 03:47:01 +0000

    Make new storage for manual (PDF) submissions

--- web-server/grade_gator/storage_backends.py
+++ web-server/grade_gator/storage_backends.py
@@ -8,3 +8,11 @@ class ProfessorTestCasesStorage(S3Boto3Storage):
 
 class ManualUngradedStorage(S3Boto3Storage):
      bucket_name = 'manual-ungraded-submissions'
+     region_name = 'us-east-2'
+     querystring_auth = True  
+     querystring_expire = 3600  # optional, URL valid for 1 hour
+     object_parameters = {
+        'ContentType': 'application/pdf',
+        'ContentDisposition': 'inline',  # this forces PDF preview
+     }
+     default_acl = 'private'
